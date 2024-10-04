ca47d1d687c40146a8945990cf8a7bfdf0730ef5
ing raul <ing.raul.santiago30@gmail.com>
2024-10-04 16:03:45 -0400

    se quito un boton

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -15,7 +15,7 @@ export default function ServicesPage() {
 
     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis{" "} <span className="font-bold text-secondary"> servicios.</span></h1>
     <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS, JavaScript, React, Nextjs, Angular, Php y Laravel diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
-    <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
+   
     </div>
      {/* SLIDER */}
      <div>
